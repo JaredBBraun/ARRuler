69f8295a8977a9c207fb4f1e3db124648ff6d49b
Jared Braun <jaredbbraun@gmail.com>
2017-08-17 16:13:26 -0400

    doesnt fully work
    tried to do sphere moving with drag

---
+++ ARTableSet/Assets/Scripts/DragObject.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragObject : MonoBehaviour
+{
+    private Camera mainCam;
+    private GameObject go;
+    [SerializeField]
+    float _horizontallimit = 10f, _verticallimit = 10f, dragSpeed = 0.1f;
+
+    Transform cachedTransform;
+    Vector3 startingPos;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //referencia transform
+        cachedTransform = transform;
+
+        //salva a posicao inicial
+        startingPos = cachedTransform.position;
+
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Vector2 deltaPosition = Input.GetTouch(0).deltaPosition;
+            switch (Input.GetTouch(0).phase)
+            {
+                case TouchPhase.Began:
+                    break;
+
+                case TouchPhase.Moved:
+                    RaycastHit hit = new RaycastHit();
+                    for (int i = 0; i < Input.touchCount; ++i)
+                    {
+                        Ray ray = mainCam.ScreenPointToRay(Input.GetTouch(i).position);
+                        if (Physics.Raycast(ray, out hit))
+                        {
+                            if (hit.transform.gameObject.CompareTag ("glass"))
+                            {
+                                go = hit.transform.gameObject;
+                                DragObj(deltaPosition, go);
+                                go = null;
+                            }
+                        }
+
+                    }
+                        
+                    break;
+
+                case TouchPhase.Ended:
+                    break;
+            }
+        }
+    }
+
+    void DragObj(Vector2 deltaPos, GameObject go)
+    {
+        go.transform.position = new Vector3(Mathf.Lerp((deltaPos.x * dragSpeed) + go.transform.position.x, startingPos.x - _horizontallimit, startingPos.x + _horizontallimit),
+                                                           
+                                                           
+            (go.transform.position.y),
+
+            Mathf.Clamp((deltaPos.y * dragSpeed) + go.transform.position.z, startingPos.z - _verticallimit, startingPos.z + _verticallimit) );
+       
+    }
+}
--- ARTableSet/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ ARTableSet/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -5,25 +5,49 @@ Confidential and Proprietary - Protected under copyright and other laws.
 ==============================================================================*/
 
 using UnityEngine;
+using TMPro;
 
 namespace Vuforia
 {
     /// <summary>
     /// A custom handler that implements the ITrackableEventHandler interface.
     /// </summary>
-    public class DefaultTrackableEventHandler : MonoBehaviour,
-                                                ITrackableEventHandler
+    public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
     {
+        [System.Serializable]
+        public enum STATE
+        {
+            IDLE,
+            PICTURE,
+            PLACING,
+            GLASS
+        }
+        public TextMeshProUGUI tm;
+        public STATE state = STATE.PICTURE;
+        public Camera mainCam;
+
+        public Renderer quad;
+        public Renderer bgquad;
+
+        [SerializeField]
+        float _horizontallimit = 2.5f, _verticallimit = 0f, dragSpeed = 0.1f;
+
+        Transform cachedTransform;
+        Vector3 startingPos;
+
         #region PRIVATE_MEMBER_VARIABLES
- 
+
         private TrackableBehaviour mTrackableBehaviour;
-    
+        private bool takeHiResShot = true;
+        private int resWidth = 1920;
+        private int resHeight = 1080;
+
         #endregion // PRIVATE_MEMBER_VARIABLES
 
 
 
         #region UNTIY_MONOBEHAVIOUR_METHODS
-    
+
         void Start()
         {
             mTrackableBehaviour = GetComponent<TrackableBehaviour>();
@@ -68,6 +92,7 @@ namespace Vuforia
 
         private void OnTrackingFound()
         {
+            #region default found
             Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
             Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
 
@@ -84,11 +109,17 @@ namespace Vuforia
             }
 
             Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+
+            #endregion
+
+            tm.text = "Target Found";
+            state = STATE.PLACING;
         }
 
 
         private void OnTrackingLost()
         {
+            #region default lost
             Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
             Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
 
@@ -105,8 +136,89 @@ namespace Vuforia
             }
 
             Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            #endregion
+            tm.text = "Target Lost";
+            //state = STATE.IDLE;
         }
 
         #endregion // PRIVATE_METHODS
+
+        private void Update()
+        {
+            if (state == STATE.PLACING)
+            {
+                tm.text = "Tap Your Table to Place Glass";
+                RaycastHit hit = new RaycastHit();
+                for (int i = 0; i < Input.touchCount; ++i)
+                {
+
+                    // Construct a ray from the current touch coordinates
+                    Ray ray = mainCam.ScreenPointToRay(Input.GetTouch(i).position);
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (Input.GetTouch(i).phase == TouchPhase.Began)
+                        {
+                            //hit.transform.gameObject.SendMessage("OnMouseDown");
+                            if (hit.transform.gameObject.name == "Plane")
+                            {
+                                //crosshair.transform.position = new Vector3(
+                                //    
+                                //    hit.point.x,
+                                //    hit.point.y,
+                                //    hit.point.z); // Input.GetTouch(i).position;
+
+                                GameObject go = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere),
+                                    new Vector3(hit.point.x, hit.point.y, hit.point.z),
+                                    Quaternion.identity) as GameObject;
+                                go.tag = "glass";
+                                go.transform.localScale = new Vector3(go.transform.localScale.x * 7f,
+                                    go.transform.localScale.y * 7f,
+                                    go.transform.localScale.z * 7f);
+
+                                go.AddComponent<DragObject>();
+                                var PLANE = hit.transform.gameObject;
+                            }
+                        }
+                        
+                                               
+                    }
+
+
+
+                }
+            }
+
+
+            if (state == STATE.PICTURE)
+            {
+                tm.text = "Touch Screen to Take Picture";
+                if (Input.touchCount > 0)
+                {
+                    if (takeHiResShot)
+                    {
+                        quad.material.mainTexture = bgquad.material.mainTexture;
+
+                        takeHiResShot = false;
+                        state = STATE.IDLE;
+                    }
+
+                }
+            }
+            if (state == STATE.IDLE)
+            {
+                tm.text = "Make Your Camera Look at Your Coupon";
+            }
+        }
+
+        void DragObj(Vector2 deltaPos , GameObject go)
+        {
+            go.transform.position = new Vector3(Mathf.Clamp((deltaPos.x * dragSpeed) + go.transform.position.x,
+                                                               startingPos.x - _horizontallimit, startingPos.x + _horizontallimit),
+                                                               Mathf.Clamp((deltaPos.y * dragSpeed) + go.transform.position.y,
+                                                               startingPos.y - _verticallimit, startingPos.y + _verticallimit),
+                                                                go.transform.position.z);
+        }
+
+
     }
 }
